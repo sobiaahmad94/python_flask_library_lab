f1b73a1efe6cce1663e0ec886787711a4f88304f
sobiaahmad94 <sobiaahmad94@icloud.com>
2023-04-22 16:42:41 +0100

    Changed to a html table and added a proper navbar

--- controllers/controller.py
+++ controllers/controller.py
@@ -3,15 +3,15 @@ from flask import Flask, render_template, request
 # from the file app.py, import the variable app
 from models.books import *
 
-@app.route('/')
+@app.route('/home')
 def index():
-    return render_template("index.jinja", title = "Homepage")
+    return render_template("index.jinja", title = "Home")
 
 
 @app.route('/display_all_books')
 def display_all_books():
     # all_books = books_list
-    return render_template("all_books.jinja", title = "All Books", books_list = books_list)
+    return render_template("all_books.jinja", title = "Display All Books", books_list = books_list)
 
 # display_all_books()
 
--- models/books.py
+++ models/books.py
@@ -1,7 +1,7 @@
 from models.book import *
 
 book_1 = Book("The Third Policeman", "Flann O'Brien", "eccentric humour")
-book_2 = Book("In Cold Blood", "Truman Capote", "crime")
+book_2 = Book("In Cold Blood", "Truman Capote", "true crime")
 book_3 = Book("Brave New World", "Aldous Huxley", "dystopian sci-fi")
 
 books_list = [book_1, book_2, book_3]
